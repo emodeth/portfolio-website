44a2f129627eadc572d179d3cdd7f1c9dfad5f92
emodeth <emodethistaken@gmail.com>
2023-07-30 22:17:27 +0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import "./footer.scss";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <div className="footer__content">
+          <p className="footer__copyright">
+            Copyright © 2023. All rights are reserved
+          </p>
+          <div class="footer__socials">
+            <a
+              aria-label="linkedin"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/emirhan-keskin-99854b215/"
+            >
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a
+              aria-label="github"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/emodeth"
+            >
+              <i class="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
