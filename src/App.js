0d655a9e1e8696f13f77337899d0a5e675d1aec5
emodeth <emodethistaken@gmail.com>
2023-07-30 22:04:27 +0300

    Contact section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import "./contact.scss";
+
+function Contact() {
+  return (
+    <div className="contact" id="contact">
+      <div className="contact__container container">
+        <h3 className="contact__header">Contact</h3>
+        <h4 className="contact__header-2">Don't be shy! Hit me up! 👇</h4>
+        <div className="contact__contacts">
+          <div className="contact__location">
+            <span className="contact__location-icon">
+              <i class="fa-solid fa-map-location-dot"></i>
+            </span>
+            <div className="contact__location-content">
+              <h5 className="contact__location-header">Location</h5>
+              <p className="contact__location-text">Istanbul, Turkey</p>
+            </div>
+          </div>
+          <div className="contact__mail">
+            <span className="contact__mail-icon">
+              <i class="fa-solid fa-envelope-open-text"></i>
+            </span>
+            <div className="contact__mail-content">
+              <h5 className="contact__mail-header">Mail</h5>
+              <p className="contact__mail-text">
+                <a href="mailto:emirhankeskin9119@gmail.com">
+                  emirhankeskin9119@gmail.com
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
